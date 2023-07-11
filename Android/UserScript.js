5e172ce13a0f9ee23a39c7c0c0fcf49689b7a01c
Leͥgeͣnͫd <46375777+longthinh@users.noreply.github.com>
2023-07-11 19:43:50 +0700

    Update UserScript.js

--- Android/UserScript.js
+++ Android/UserScript.js
@@ -9,7 +9,7 @@
 // @grant        none
 // @run-at       document-start
 // @homepage     https://longthinh.github.io/
-// @downloadURL  https://github.com/longthinh/longthinh.github.io/releases/download/UserScript/UserScript.js
+// @downloadURL  https://longthinh.github.io/releases/download/UserScript/UserScript.js
 // ==/UserScript==
 /// @stats blockers for 6069 domains, injected CSS rules for 0 domains
 {
